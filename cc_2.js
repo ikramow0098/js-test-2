b0d527df037c6da3247fe42f966112c1dc6fae1c
Fotihjon <ikromov@usf.edu>
2025-01-24 01:02:29 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -2,3 +2,14 @@ let products = ["Apple", "Banana", "Kiwi", "Apricot", "Pineapple"];
 products.push("Watermelon");
 products.pop();
 console.log(products);
+
+let scores = [88, 95, 76, 87, 92];
+scores[1] = 65;
+let total = 0;
+for (let i = 0; i < scores.length; i++) {
+  total += scores[i];
+}
+const average = total / scores.length;
+
+console.log(scores)
+console.log("Average score:", average);
